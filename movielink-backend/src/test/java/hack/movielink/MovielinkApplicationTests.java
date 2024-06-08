7abe8f74f7434fbc611a6961c05712fdc319d96a
stefanfaur <stefan.faur@student.upt.ro>
2024-06-08 10:24:23 +0300

    Initial commit.

---
+++ movielink-backend/src/test/java/hack/movielink/MovielinkApplicationTests.java
@@ -0,0 +1,13 @@
+package hack.movielink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
